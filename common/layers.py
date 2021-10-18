1d3154f298426bbe79b72444696661f1de8cf67c
dongyoung0 <wmkimdy@gmail.com>
2021-10-18 23:10:22 +0900

    Change file directories

---
+++ common/layers.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.5
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# basic linear neural net
+import numpy as np
+from common.funcs import cross_entropy_error, softmax
+
+
+class MatMul:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.x = None
+        
+    def forward(self, x):
+        W, = self.params
+        out = np.matmul(x, W)
+        self.x = x
+        return out
+
+    def backward(self, dout):
+        W, = self.params
+        dx = np.matmul(dout, W.T)
+        dW = np.matmul(self.x.T, dout)
+        self.grads[0][...] = dW
+        return dx
+
+
+class Sigmoid:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.out = None
+        
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+    
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+        return dx        
+
+
+# +
+class Affine1:
+    def __init__(self, W, b):
+        self.layer = MatMul(W)
+        self.params = W, b
+        self.grads = [np.zeros_like(W), np.zeros_like(b)]
+        self.x = None
+        
+    def forward(self, x):
+        _, b = self.params
+        out = self.layer.forward(x)
+        self.x = x
+        return out
+    
+    def backward(self, dout):
+        dx = self.layer.backward(dout)
+        dW = self.layer.grads
+        db = np.sum(dout, axis=0)
+        
+        self.grads[0][...] = dW
+        self.grads[1][...] = db
+        return dx
+    
+
+class Affine:
+    def __init__(self, W, b):
+        self.params = [W, b]
+        self.grads = [np.zeros_like(W), np.zeros_like(b)]
+        self.x = None
+
+    def forward(self, x):
+        W, b = self.params
+        out = np.dot(x, W) + b
+        self.x = x
+        return out
+
+    def backward(self, dout):
+        W, b = self.params
+        dx = np.dot(dout, W.T)
+        dW = np.dot(self.x.T, dout)
+        db = np.sum(dout, axis=0)
+
+        self.grads[0][...] = dW
+        self.grads[1][...] = db
+        return dx
+# -
+
+
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.y = None  # softmax의 출력
+        self.t = None  # 정답 레이블
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+
+        # 정답 레이블이 원핫 벡터일 경우 정답의 인덱스로 변환
+        if self.t.size == self.y.size:
+            self.t = self.t.argmax(axis=1)
+
+        loss = cross_entropy_error(self.y, self.t)
+        return loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+
+        dx = self.y.copy()
+        dx[np.arange(batch_size), self.t] -= 1
+        dx *= dout
+        dx = dx / batch_size
+
+        return dx
