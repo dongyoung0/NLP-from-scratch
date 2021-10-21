881a4e95fb48898a54e4ff617959646e46f4fc17
dongyoung0 <wmkimdy@gmail.com>
2021-10-21 16:08:18 +0900

    Update common files and finish chapter 3 practice

--- common/trainer.py
+++ common/trainer.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import sys
 sys.path.append('..')
 import numpy as np
@@ -45,7 +46,7 @@ class Trainer:
                 if (eval_interval is not None) and (iters % eval_interval) == 0:
                     avg_loss = total_loss / loss_count
                     elapsed_time = time.time() - start_time
-                    print(f'| epoch {current_epoch+1} |  itr {iters+1}/{max_iters} | time {elapsed_time}[s] | loss {avg_loss}')
+                    print(f'| epoch {self.current_epoch+1} |  itr {iters+1}/{max_iters} | time {elapsed_time}[s] | loss {avg_loss}')
                     self.loss_list.append(float(avg_loss))
                     total_loss, loss_count = 0, 0
 
@@ -59,3 +60,38 @@ class Trainer:
         plt.xlabel('itr (x' + str(self.eval_interval) + ')')
         plt.ylabel('loss')
         plt.show()
+
+
+def remove_duplicate(params, grads):
+    '''
+    매개변수 배열 중 중복되는 가중치를 하나로 모아
+    그 가중치에 대응하는 기울기를 더한다.
+    '''
+    params, grads = params[:], grads[:]  # copy list
+
+    while True:
+        find_flg = False
+        L = len(params)
+
+        for i in range(0, L - 1):
+            for j in range(i + 1, L):
+                # 가중치 공유 시
+                if params[i] is params[j]:
+                    grads[i] += grads[j]  # 경사를 더함
+                    find_flg = True
+                    params.pop(j)
+                    grads.pop(j)
+                # 가중치를 전치행렬로 공유하는 경우(weight tying)
+                elif params[i].ndim == 2 and params[j].ndim == 2 and \
+                     params[i].T.shape == params[j].shape and np.all(params[i].T == params[j]):
+                    grads[i] += grads[j].T
+                    find_flg = True
+                    params.pop(j)
+                    grads.pop(j)
+
+                if find_flg: break
+            if find_flg: break
+
+        if not find_flg: break
+
+    return params, grads
