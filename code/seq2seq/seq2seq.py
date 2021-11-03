cc2d0158f6116e42946cd55e6b775b0337fb5342
dongyoung0 <wmkimdy@gmail.com>
2021-11-03 21:29:23 +0900

    Create implementation of seq2seq

---
+++ code/seq2seq/seq2seq.py
@@ -0,0 +1,235 @@
+import sys
+sys.path.append('../,,')
+from common.base_model import BaseModel
+from common.time_layers import *
+
+class Encoder:
+    def __init__(self, vocab_size, wordvec_size, hidden_size):
+        '''
+        vocab_size : 어휘 수
+        wordvec_size : word vector의 dimension
+        hidden_size : LSTM layer의 hidden state vector의 dimension
+        '''
+        V, D, H = vocab_size, wordvec_size, hidden_size
+        rn = np.random.randn
+        
+        embed_W = (rn(V, D)/100).astype('f')
+        lstm_Wx = (rn(D, 4*H)/np.sqrt(D)).astype('f')
+        lstm_Wh = (rn(H, 4*H)/np.sqrt(H)).astype('f')
+        lstm_b = np.zeros(4*H).astype('f')
+        
+        self.embed = TimeEmbedding(embed_W)
+        self.lstm = TimeLSTM(lstm_Wx, lstm_Wh, lstm_b, stateful=False)
+        
+        self.params = self.embed.params + self.lstm.params
+        self.grads = self.embed.grads + self.lstm.grads
+        self.hs = None
+    
+    def forward(self, xs):
+        xs = self.embed.forward(xs)
+        hs = self.lstm.forward(xs)
+        self.hs = hs
+        return hs[:,-1,:]
+    
+    def backward(self, dh):
+        dhs = np.zeros_like(self.hs)
+        dhs[:, -1, :] = dh
+        
+        dout = self.lstm.backward(dhs)
+        dout = self.embed.backward(dout)
+        return dout
+        
+class Decoder:
+    def __init__(self, vocab_size, wordvec_size, hidden_size):
+        V, D, H = vocab_size, wordvec_size, hidden_size
+        rn = np.random.randn
+        
+        embed_W = (rn(V, D)/100).astype('f')
+        lstm_Wx = (rn(D, 4*H)/np.sqrt(D)).astype('f')
+        lstm_Wh = (rn(H, 4*H)/np.sqrt(H)).astype('f')
+        lstm_b = np.zeros(4*H).astype('f')
+        affine_W = (rn(H,V)/np.sqrt(H)).astype('f')
+        affine_b = np.zeros(V).astype('f')
+        
+        self.embed = TimeEmbedding(embed_W)
+        self.lstm = TimeLSTM(lstm_Wx, lstm_Wh, lstm_b, stateful=True)
+        self.affine = TimeAffine(affine_W, affine_b)
+        self.params, self.grads = [], []
+        
+        for layer in (self.embed, self.lstm, self.affine):
+            self.params += layer.params
+            self.grads += layer.grads
+            
+        
+    def forward(self, xs, h):
+        self.lstm.set_state(h)
+        
+        out = self.embed.forward(xs)
+        out = self.lstm.forward(out)
+        score = self.affine.forward(out)
+        return score
+
+    
+    def backward(self, dscore):
+        dout = self.affine.backward(dscore)
+        dout = self.lstm.backward(dout)
+        dout = self.embed.backward(dout)
+        dh = self.lstm.dh
+        return dh
+    
+    def generate(self, h, start_id, sample_size):
+        '''
+        위 forward는 학습 시 사용
+        generate는 새 문장을 생성할 때 사용
+        '''
+        sampled = []
+        sample_id = start_id
+        self.lstm.set_state(h)
+        
+        for _ in range(sample_size):
+            x = np.array(sample_id).reshape((1, 1))
+            out = self.embed.forward(x)
+            out = self.lstm.forward(out)
+            score = self.affine.forward(out)
+            
+            sample_id = np.argmax(score.flatten())
+            sampled.append(int(sample_id))
+            
+        return sampled
+    
+class Seq2seq(BaseModel):
+    def __init__(self, vocab_size, wordvec_size, hidden_size):
+        V, D, H = vocab_size, wordvec_size, hidden_size
+        self.encoder = Encoder(V, D, H)
+        self.decoder = Decoder(V, D, H)
+        self.softmax = TimeSoftmaxWithLoss()
+        
+        self.params = self.encoder.params + self.decoder.params
+        self.grads = self.encoder.grads + self.decoder.grads
+    
+    def forward(self, xs, ts):
+        decoder_xs, decoder_ts = ts[:, :-1], ts[:, 1:]
+        h = self.encoder.forward(xs)
+        score = self.decoder.forward(decoder_xs, h)
+        loss = self.softmax.forward(score, decoder_ts)
+        return loss
+    
+    def backward(self, dout=1):
+        dout = self.softmax.backward(dout)
+        dh = self.decoder.backward(dout)
+        dout = self.encoder.backward(dh)
+        return dout
+    
+    def generate(self, xs, start_id, sample_size):
+        h = self.encoder.forward(xs)
+        sampled = self.decoder.generate(h, start_id, sample_size)
+        return sampled
+
+class PeekyDecoder:
+    def __init__(self, vocab_size, wordvec_size, hidden_size):
+        V, D, H = vocab_size, wordvec_size, hidden_size
+        rn = np.random.randn
+        
+        embed_W = (rn(V, D)/100).astype('f')
+        # hidden 추가
+        lstm_Wx = (rn(H + D, 4 * H) / np.sqrt(H + D)).astype('f')
+        lstm_Wh = (rn(H, 4 * H) / np.sqrt(H)).astype('f')
+        lstm_b = np.zeros(4 * H).astype('f')
+        #hidden 추가
+        affine_W = (rn(H + H, V) / np.sqrt(H + H)).astype('f')
+        affine_b = np.zeros(V).astype('f')
+        
+        self.embed = TimeEmbedding(embed_W)
+        self.lstm = TimeLSTM(lstm_Wx, lstm_Wh, lstm_b, stateful=True)
+        self.affine = TimeAffine(affine_W, affine_b)
+        
+        self.params, self.grads = [], []
+        for layer in (self.embed, self.lstm, self.affine):
+            self.params += layer.params
+            self.grads += layer.grads
+        self.cache = None
+        
+    def forward(self, xs, h):
+        N, T = xs.shape
+        N, H = h.shape
+        
+        self.lstm.set_state(h)
+        
+        out = self.embed.forward(xs)
+        # 추가된 hidden h와 기존에 전달되던 out을 concat
+        hs = np.repeat(h, T, axis=0).reshape(N, T, H)
+        out = np.concatenate((hs, out), axis=2)
+        
+        out = self.lstm.forward(out)
+        # 위와 마찬가지로 h 추가
+        out = np.concatenate((hs, out), axis=2)
+        
+        score = self.affine.forward(out)
+        self.cache = H
+        return score
+    
+    def backward(self, dscore):
+        H = self.cache
+
+        dout = self.affine.backward(dscore)
+        # 추가된 h 반영
+        dout, dhs0 = dout[:, :, H:], dout[:, :, :H]
+        dout = self.lstm.backward(dout)
+        # 추가된 h 반영2
+        dembed, dhs1 = dout[:, :, H:], dout[:, :, :H]
+        self.embed.backward(dembed)
+        
+        # 추가된 h 반영3
+        dhs = dhs0 + dhs1
+        dh = self.lstm.dh + np.sum(dhs, axis=1)
+        return dh
+
+    def generate(self, h, start_id, sample_size):
+        sampled = []
+        char_id = start_id
+        self.lstm.set_state(h)
+
+        H = h.shape[1]
+        peeky_h = h.reshape(1, 1, H)
+        for _ in range(sample_size):
+            x = np.array([char_id]).reshape((1, 1))
+            out = self.embed.forward(x)
+
+            out = np.concatenate((peeky_h, out), axis=2)
+            out = self.lstm.forward(out)
+            out = np.concatenate((peeky_h, out), axis=2)
+            score = self.affine.forward(out)
+
+            char_id = np.argmax(score.flatten())
+            sampled.append(char_id)
+
+        return sampled    
+    
+class PeekySeq2seq(Seq2seq):
+    def __init__(self, vocab_size, wordvec_size, hidden_size):
+        V, D, H = vocab_size, wordvec_size, hidden_size
+        self.encoder = Encoder(V, D, H)
+        self.decoder = PeekyDecoder(V, D, H)
+        self.softmax = TimeSoftmaxWithLoss()
+        
+        self.params = self.encoder.params + self.decoder.params
+        self.grads = self.encoder.grads + self.decoder.grads
+    
+    def forward(self, xs, ts):
+        decoder_xs, decoder_ts = ts[:, :-1], ts[:, 1:]
+        h = self.encoder.forward(xs)
+        score = self.decoder.forward(decoder_xs, h)
+        loss = self.softmax.forward(score, decoder_ts)
+        return loss
+    
+    def backward(self, dout=1):
+        dout = self.softmax.backward(dout)
+        dh = self.decoder.backward(dout)
+        dout = self.encoder.backward(dh)
+        return dout
+    
+    def generate(self, xs, start_id, sample_size):
+        h = self.encoder.forward(xs)
+        sampled = self.decoder.generate(h, start_id, sample_size)
+        return sampled
+        
